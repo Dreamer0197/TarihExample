18725c62cdcca8c0800d08dc376a8b3c600f5da3
Kaan Şengün <427642@ogr.ktu.edu.tr>
2023-11-14 21:05:52 +0300

    Finish

---
+++ TarihExample/Date.h
@@ -0,0 +1,12 @@
+//
+//  Date.h
+//  TarihExample
+//
+//  Created by Kaan Şengün on 14.11.2023.
+//
+
+#ifndef Date_h
+#define Date_h
+
+
+#endif /* Date_h */
---
+++ TarihExample/Tarih.h
@@ -0,0 +1,12 @@
+//
+//  Tarih.h
+//  TarihExample
+//
+//  Created by Kaan Şengün on 14.11.2023.
+//
+
+#ifndef Tarih_h
+#define Tarih_h
+
+
+#endif /* Tarih_h */
